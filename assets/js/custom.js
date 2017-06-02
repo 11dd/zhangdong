c41db9c9fcf4ceab57b4076ca3c15ba36335e87c
zdd8746 <jsjzdd_2014@126.com>
2017-06-03 01:06:28 +0800

    113

---
+++ assets/js/custom.js
@@ -0,0 +1,66 @@
+﻿
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        scroll_fun: function () {
+
+            /*====================================
+             SCROLL SCRIPTS 
+            ======================================*/
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+
+            
+
+        },
+         menu_fun: function () {
+
+            /*====================================
+                 EASING PLUGIN SCRIPTS 
+                ======================================*/
+            $(function () {
+                $("#menu-close").click(function (e) {
+                    e.preventDefault();
+                    $("#sidebar-wrapper").toggleClass("active");
+                });
+                $("#menu-button").click(function (e) {
+                    e.preventDefault();
+                    $("#sidebar-wrapper").toggleClass("active");
+                });
+
+            });
+
+        },
+
+        
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.scroll_fun();
+        mainApp.menu_fun();
+        mainApp.custom_fun();
+    });
+}(jQuery));
+
+
